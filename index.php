b58b76f962d39204d0ce52de305846d9a743b560
tubagus ahmad <tubagus.ahmad@gmail.com>
2014-03-07 16:21:57 +0700

    Delete index.php

--- index.php
+++
@@ -1,7 +0,0 @@
-<?php 
-session_start();
-if(isset($_SESSION['username']) && $_SESSION['authorized'] == 1){
-	echo ("<script type='text/javascript'>document.location='admin.php';</script>");
-}else{
-	include"login.php";
-}?>
